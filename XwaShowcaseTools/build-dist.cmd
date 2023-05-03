d14450cef33dfa2ab1cfa71553dbdb8990cd8cdb
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-05-03 17:36:03 +0200

    Update build-dist.cmd

--- XwaShowcaseTools/build-dist.cmd
+++ XwaShowcaseTools/build-dist.cmd
@@ -3,11 +3,4 @@ setlocal
 
 cd "%~dp0"
 
-For %%a in (
-"XwaOptShowcase\bin\Release\net6.0-windows\*.dll"
-"XwaOptShowcase\bin\Release\net6.0-windows\*.exe"
-"XwaOptShowcase\bin\Release\net6.0-windows\*.json"
-"XwaOptShowcase\bin\Release\net6.0-windows\*.cso"
-) do (
-xcopy /s /d "%%~a" dist\
-)
+xcopy /s /d "XwaOptShowcase\bin\Release\net6.0-windows\" dist\
